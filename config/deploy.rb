9e3f7fe3b6d201211de481d1570c78d65600fa76
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-04-27 16:36:28 +0200

    Cambios en Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,13 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 set :rvm_ruby_string, 'ruby-1.9.3-p125'
-set :rvm_type, :user
 require "bundler/capistrano"
-set :application, "gestionclinica"
+set :application, "corporativeweb"
 set :domain, "mail.galiclick.com"
 set :user, "galiclick"
-set :repository,  "git@gitorious.org:galiclick/gestionclinica.git"
+set :port, 61323
+set :repository,  "git@github.com:jgamsan/corporativeweb.git"
 set :scm, :git
-set :keep_releases, 3
+set :keep_releases, 2
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 set :use_sudo, false
@@ -16,15 +15,9 @@ set :deploy_via, :remote_cache
 set :deploy_to, "/home/galiclick/public_html/#{application}"
 set :rails_env, "production"
 
-role :web, "#{domain}:53877"
-role :app, "#{domain}:53877"
-role :db,  "#{domain}:53877", :primary => true
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
 
-namespace :customs do
-  task :symlink, :roles => :app do
-    run "ln -nfs #{shared_path}/system/uploads #{release_path}/public"
-  end
-end
-after "deploy:create_symlink","customs:symlink"
 after "deploy", "deploy:cleanup"
 require 'capistrano-unicorn'
